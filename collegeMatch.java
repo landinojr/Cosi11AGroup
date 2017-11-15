fa82cfb5883742d3dbae353c69953b7216af155d
luisandino <luisandinojr1@brandeis.edu>
2017-11-14 21:51:44 -0500

    can read a file and enter it to an array

--- collegeMatch.java
+++ collegeMatch.java
@@ -1,7 +1,32 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 import java.util.*;
 
 public class collegeMatch{
-  public static void main (String [] args){
-
+  public static void main (String [] args) throws FileNotFoundException{
+    String[][] convertedFile = readFile();
+    test(convertedFile);
   }
+
+  public static String [][] readFile() throws FileNotFoundException{
+    File file = new File("FinalData.csv");
+
+    Scanner console = new Scanner(file);
+    console.next();
+    int dataSize = 1492;
+    String[][] stats = new String[dataSize][9];
+    console.nextLine();
+    int pos=0;
+     while(console.hasNext()){
+       String line = console.nextLine();
+       String[] fields = line.split(","); // split the columns apart
+       for(int i = 0; i < fields.length; i++){
+          stats[pos][i] = fields[i];
+       }
+       pos++;
+     }
+     return stats;
+  }
+
 }
